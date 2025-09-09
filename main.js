606fbee74aa8d99f77276d4dc44f3f044b188493
Fouad146 <fuadmustafa146@gmail.com>
2025-09-09 16:23:09 +0300

    s-commit

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ bootStrap(app,express)
 
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send({message:'Hello World!'}))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 // postMan
